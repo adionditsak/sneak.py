fc317bf29d1692cb9ea7ea75320be47e1948f042
adionditsak <aarvik92@gmail.com>
2014-07-20 13:19:52 +0200

    Init changes

--- install.sh
+++ install.sh
@@ -8,17 +8,17 @@ unzip master.zip
 
 cd sneak.py-master
 
-"Creating /usr/local/bin/sneak/"
+echo "Creating /usr/local/bin/sneak/"
 mkdir -p /usr/local/bin/sneak/
 
-"Copying sneak.py to /usr/local/bin/sneak/ and sneek (init) to /etc/init.d/"
+echo "Copying sneak.py to /usr/local/bin/sneak/ and sneek (init) to /etc/init.d/"
 cp sneak /etc/init.d/sneak
 cp sneak.py /usr/local/bin/sneak/sneak.py
 
-"Making files executable"
+echo "Making files executable"
 chmod +x /etc/init.d/sneak /usr/local/bin/sneak/sneak.py
 
-"Removing zip and git directory"
+echo "Removing zip and git directory"
 rm -rf sneak.py-master/ master.zip
 
 sleep 2
