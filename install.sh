ee64e64be75f53f9fb700ac6fc3a11a38a7da91f
Anders <aarvik92@gmail.com>
2014-07-20 12:36:06 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-curl -OL https://github.com/adionditsak/sneak.py/archive/master.zip
-unzip master.zip
-cd sneak.py-master
-mkdir -p /usr/local/bin/sneak/
-cp sneak /etc/init.d/sneak
-cp sneak.py /usr/local/bin/sneak/sneak.py
-chmod +x /etc/init.d/sneak /usr/local/bin/sneak/sneak.py
