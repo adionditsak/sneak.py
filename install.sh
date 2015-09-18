5910acbfa88f88ab2df1e5b89dc732063154d14a
Anders Aarvik <aarvik92@gmail.com>
2015-09-18 16:04:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,31 +15,32 @@ cecho(){
 }
 
 echo ""
-echo "Downloading sneak.py" | cecho LGREEN
+echo "- Downloading sneak.py" | cecho LGREEN
 curl -OL https://github.com/adionditsak/sneak.py/archive/master.zip
 
 echo ""
-echo "Unzipping sneak.py" | cecho LGREEN
+echo "- Unzipping sneak.py" | cecho LGREEN
 unzip master.zip
 
 cd sneak.py-master;
 
 echo ""
-echo "Creating /usr/local/bin/sneak/" | cecho LGREEN
+echo "- Creating /usr/local/bin/sneak/ directory" | cecho LGREEN
 mkdir -p /usr/local/bin/sneak/
 
 echo ""
-echo "Copying sneak.py to /usr/local/bin/sneak/ and sneek (init) to /etc/init.d/" | cecho LGREEN
+echo "- Copying sneak.py to /usr/local/bin/sneak/ and sneak (init) to /etc/init.d/" | cecho LGREEN
 cp sneak /etc/init.d/sneak
 cp sneak.py /usr/local/bin/sneak/sneak.py
 
 echo ""
-echo "Making files executable" | cecho LGREEN
+echo "- CHMODDING files" | cecho LGREEN
 chmod +x /etc/init.d/sneak /usr/local/bin/sneak/sneak.py
 
 echo ""
-echo "Done" | cecho LGREEN
-echo "remember to remove the master.zip and the sneak.py-master files" | cecho YELLOW
+echo "- Done" | cecho LGREEN
 
 echo ""
-echo "Start with /etc/init.d/sneak start" | cecho LGREEN
+echo "- Notes:"
+echo "- Remove master.zip and sneak.py-master files from current directory" | cecho YELLOW
+echo "- Start service with $ /etc/init.d/sneak start" | cecho LGREEN
